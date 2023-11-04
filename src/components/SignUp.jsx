6b113bfcceee77d386d4ab6c64c5d1db1a379295
Joelle  Johnson <joellej221@gmail.com>
2023-11-04 18:28:40 -0400

    Trying to find a way to get data from database, check if the email already exists, then add the new user to the database if its a new email

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 
-export default function SignUp({signedUp, setSignedUp}) {
+
+export default function SignUp({ signedUp, setSignedUp }) {
     const nav = useNavigate()
+
 
     const handleFormSubmit = (e) => {
         e.preventDefault()
@@ -10,46 +12,62 @@ export default function SignUp({signedUp, setSignedUp}) {
             email: e.target.email.value,
             password: e.target.password.value
         }
-        fetch("http://localhost:8080", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(formData),
-        })
+        //get information from database
+        fetch("http://localhost:8080/")
             .then(res => res.json())
-            .then((login) => setSignedUp(true))
+            .then((users) => {
+                users.some((user) => user.email === formData.email) ?
+                    console.log('An account already exists with this email. Please log in or sign up with a different eamil.')
+                    :
+                    //add user to db 
+                    fetch("http://localhost:8080/", {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(formData),
+                    })
+                        .then(res => res.json())
+                        .then(() => {
+                            setSignedUp(true)
+                            nav("/login")
+                        })
+                        .catch(console.error)
+            })
             .catch(console.error)
+        }
 
-        nav("/login")
+    
+        
 
-    }
-    const goToLogIn = () => {
-        nav("/login")
-    }
 
-    return (
-        <>
-            <h2>Sign Up:</h2>
-            <form onSubmit={handleFormSubmit} action="">
 
-                <label htmlFor="email">
-                    Email:
-                    <input type="email" name="email" />
-                </label>
-                <br />
-                <label htmlFor="password">
-                    Password:
-                    <input type="password" name="password" />
-                </label>
-                <br />
-                <button type="submit">Sign up</button>
-                <br />
-                <hr />
-                <h3>Already have an account?</h3>
-                <button onClick={goToLogIn}>Log In</button>
+const goToLogIn = () => {
+    nav("/login")
+}
 
-            </form>
-        </>
-    )
+return (
+    <>
+        <h2>Sign Up:</h2>
+        <form onSubmit={handleFormSubmit} action="">
+
+            <label htmlFor="email">
+                Email:
+                <input type="email" name="email" />
+            </label>
+            <br />
+            <label htmlFor="password">
+                Password:
+                <input type="password" name="password" />
+            </label>
+            <br />
+            <button type="submit">Sign up</button>
+            <br />
+            <hr />
+            <h3>Already have an account?</h3>
+            <button onClick={goToLogIn}>Log In</button>
+
+        </form>
+    </>
+)
 }
